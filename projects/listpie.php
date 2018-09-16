2bd6d4b922c0ef48de9024e11b907ccce6708602
Ebru Kaya <ebky14@gmail.com>
2018-09-16 19:47:35 +0300

    initial commit

---
+++ projects/listpie.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+<title>Ebru Kaya</title>
+</head>
+<body>
+<h1>Hello world.</h1>
+</body>
+</html>
---
+++ tr_TR/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="utf-8">
+  <title>Ebru Kaya</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- FONT
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+
+  <!-- CSS
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/skeleton.css">
+
+</head>
+<body>
+
+<!-- Primary Page Layout
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<div class="container">
+  <div class="row">
+    <div class="one-half column" style="margin-top: 25%">
+      <h4>Ebru Kaya</h4>
+      <p>yakında, <a href="https://www.linkedin.com/in/ebrukaya/" target="_blank">linkedin</a></p>
+    </div>
+  </div>
+</div>
+
+<!-- End Document
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+</body>
+</html>
