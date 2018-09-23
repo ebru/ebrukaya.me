2a4ca85db4b84f32549c0e29f8a391d1af6ec93f
Ebru Kaya <ebky14@gmail.com>
2018-09-23 19:19:06 +0300

    homepage is added

--- index.php
+++ index.php
@@ -21,22 +21,38 @@
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
   <link rel="stylesheet" href="css/normalize.css">
   <link rel="stylesheet" href="css/skeleton.css">
+  <link rel="stylesheet" href="css/style.css">
 
 </head>
 <body>
 
-<!-- Primary Page Layout
-–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<div class="container">
+  <div class="row" id="navbar">
+    <div class="twelve columns"><a href="tr_TR">/tr</a></div>
+  </div>
+</div>
+
+<div class="container">
+  <section class="header">
+    <h1 class="title">ebrukaya.</h1>
+  </section>
+</div>
+
 <div class="container">
   <div class="row">
-    <div class="one-half column" style="margin-top: 25%">
-      <h4>Ebru Kaya</h4>
-      <p>soon enough, <a href="https://www.linkedin.com/in/ebrukaya/" target="_blank">linkedin</a></p>
+    <div class="seven columns">
+      <p><b>Hello!</b></p>
+      <p>I am Ebru. I graduated from Istanbul University with a degree in Computer Engineering and was an exchange student specialized in Human-Computer Interaction at Tallinn University in Estonia.</p>
+      <p>I am currently working at Sony as a Software Engineer. I have been there for almost a year and responsible for global web sites of Sony Mobile. Although working for web side for years—it all started when I was a child and it is probably one of the best clichés of my life...</p>
+      <p>I also started to learn mobile development last year as a personal interest and improved my knowledge in my free time during the year while developing an app for one of my ideas. I have recently completed and published it on the app store. It is still so new and not much smooth that I wish yet, but I hope it will be improved day by day.</p>
+      <p>Always eager to learn, saying yes to new adventures.</p>
+      <p><a href="https://www.linkedin.com/in/ebrukaya/" target="_blank"><img src="images/social/linkedin.png" style="width: 30px; padding-right: 10px" /></a><a href="https://www.instagram.com/hayallerimdeben/" target="_blank"><img src="images/social/instagram.png" style="width: 28px" /></a></p>
+    </div>
+    <div class="five columns">
+      <img src="images/pp.jpg" width="100%" />
     </div>
   </div>
 </div>
 
-<!-- End Document
-–––––––––––––––––––––––––––––––––––––––––––––––––– -->
 </body>
 </html>
--- tr_TR/index.php
+++ tr_TR/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="tr">
 <head>
 
   <!-- Basic Page Needs
@@ -19,24 +19,40 @@
 
   <!-- CSS
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/skeleton.css">
+  <link rel="stylesheet" href="../css/normalize.css">
+  <link rel="stylesheet" href="../css/skeleton.css">
+  <link rel="stylesheet" href="../css/style.css">
 
 </head>
 <body>
 
-<!-- Primary Page Layout
-–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<div class="container">
+  <div class="row" id="navbar">
+    <div class="twelve columns"><a href="../">/en</a></div>
+  </div>
+</div>
+
+<div class="container">
+  <section class="header">
+    <h1 class="title">ebrukaya.</h1>
+  </section>
+</div>
+
 <div class="container">
   <div class="row">
-    <div class="one-half column" style="margin-top: 25%">
-      <h4>Ebru Kaya</h4>
-      <p>yakında, <a href="https://www.linkedin.com/in/ebrukaya/" target="_blank">linkedin</a></p>
+    <div class="seven columns">
+      <p><b>Merhaba!</b></p>
+      <p>Ben Ebru. İstanbul Üniversitesi Bilgisayar Mühendisliği mezunuyum ve bir dönem Estonya'da Tallinn Üniversitesi'nde, İnsan Bilgisayar Etkileşimi alanında değişim öğrencisi olarak yer aldım.</p>
+      <p>Yaklaşık bir senedir Sony'de Yazılım Mühendisi olarak çalışıyorum ve Sony Mobile'ın global sitelerinin yönetildiği yazılım ekibindeyim. Web tarafıyla yıllardır ilgili olmama rağmen—her şey çocukluğumda başladı ve bu muhtemelen hayatımın en güzel klişelerinden biri...</p>
+      <p>Geçen sene ayrıca, kişisel ilgi alanlarımdan biri olarak mobil geliştirme öğrenmeye ve boş zamanlarımda aklımdaki bir fikir için uygulama geliştirmeye başladım. Kısa bir süre önce de tamamlayıp app store üzerinde yayınladım. Hala çok yeni ve tam olarak ulaşmasını istediğim düzeyde olmasa da, umarım günden güne daha da geliştirmeye devam edeceğim.</p>
+      <p>Her zaman yeni bir şeyler öğrenmeye çalışmaya, yeni maceralara evet.</p>
+      <p><a href="https://www.linkedin.com/in/ebrukaya/" target="_blank"><img src="../images/social/linkedin.png" style="width: 30px; padding-right: 10px" /></a><a href="https://www.instagram.com/hayallerimdeben/" target="_blank"><img src="../images/social/instagram.png" style="width: 28px" /></a></p>
+    </div>
+    <div class="five columns">
+      <img src="../images/pp.jpg" width="100%" />
     </div>
   </div>
 </div>
 
-<!-- End Document
-–––––––––––––––––––––––––––––––––––––––––––––––––– -->
 </body>
 </html>
