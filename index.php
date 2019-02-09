d9dbfe053581d93f2493c7e7468c5e12086da378
Ebru Kaya <ebky14@gmail.com>
2019-02-09 21:28:07 +0300

    experience, education and skills are added

--- index.php
+++ index.php
@@ -1,77 +1,205 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-
-  <!-- Basic Page Needs
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <meta charset="utf-8">
-  <title>Ebru Kaya</title>
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <!-- Mobile Specific Metas
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <!-- FONT
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
-
-  <!-- CSS
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/skeleton.css">
-  <link rel="stylesheet" href="css/style.css">
-
-  <!-- Scripts
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-  <script src="js/site.js"></script>
-
+    <!-- Basic Page Needs
+      –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <meta charset="utf-8">
+    <title>Ebru Kaya</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Mobile Specific Metas
+      –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- FONT
+      –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+    <!-- CSS
+      –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/skeleton.css">
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Scripts
+      –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="js/site.js"></script>
 </head>
+
 <body>
-<!-- Language selection -->
-<div class="container">
-  <div class="row" id="language">
-    <div class="twelve columns"><a href="/tr">/tr</a></div>
-  </div>
-</div>
-
-<div class="container">
-  <div class="row">
-
-    <!-- Intro section -->
-    <div class="seven columns">
-      <h3><b>Hi! I'm Ebru.</b></h3>
-      <h5>Software Engineer at Sony, backpacker and lover of nature 🍃</h5>
-      <p>I graduated from Istanbul University with a degree in Computer Engineering and was an exchange student specialized in Human-Computer Interaction at Tallinn University in Estonia.</p>
-      <p>I am currently working at Sony as a Software Engineer. I have been there for a year and in the team of global web sites of Sony Mobile. Personally interested in mobile development and have an app published on the app store.</p>
-      <p>Always eager to learn, saying yes to new adventures.</p>
-      <p><a href="https://www.linkedin.com/in/ebrukaya/" target="_blank"><img src="images/social/linkedin.png" style="width: 30px; padding-right: 9px" /></a><a href="https://www.instagram.com/hayallerimdeben/" target="_blank"><img src="images/social/instagram.png" style="width: 28px; padding-right: 10px" /></a><a href="https://www.github.com/ebrukye" target="_blank"><img src="images/social/github.png" style="width: 28px" /></a></p>
-    </div>
-    <div class="five columns">
-      <img src="images/pp.jpg" width="100%" />
-    </div>
-
-    <!-- Menu section -->
-    <div class="twelve columns">
-    <div class="navbar-spacer"></div>
-      <nav class="navbar">
-        <div class="container">
-          <ul class="navbar-list">
-            <li class="navbar-item"><a class="navbar-link" href="#experience">Experience</a></li>
-            <li class="navbar-item"><a class="navbar-link" href="#education">Education</a></li>
-            <li class="navbar-item"><a class="navbar-link" href="#skills">Skills</a></li>
-            <li class="navbar-item"><a class="navbar-link" href="#projects">Projects</a></li>
-            <li class="navbar-item"><a class="navbar-link" href="#personal">Personal</a></li>
-          </ul>
+    <!-- Language selection -->
+    <div class="container">
+        <div class="row" id="language">
+            <div class="twelve columns"><a href="/tr">/tr</a></div>
         </div>
-      </nav>
     </div>
-    
-  </div>
-
-</div>
-
+    <div class="container">
+        <div class="row">
+            <!-- Intro section -->
+            <div class="seven columns">
+                <h3><b>Hi! I'm Ebru.</b></h3>
+                <h5>Software Engineer at Sony, backpacker and lover of nature 🍃</h5>
+                <p>I graduated from Istanbul University with a degree in Computer Engineering and was an exchange student specialized in Human-Computer Interaction at Tallinn University in Estonia.</p>
+                <p>I am currently working at Sony as a Software Engineer. I have been there for a year and in the team of global web sites of Sony Mobile. Personally interested in mobile development and have an app published on the app store.</p>
+                <p>Always eager to learn, saying yes to new adventures.</p>
+                <p>
+                    <a href="https://www.linkedin.com/in/ebrukaya/" target="_blank"><img src="images/social/linkedin.png" style="width: 30px; padding-right: 9px" /></a>
+                    <a href="https://www.instagram.com/hayallerimdeben/" target="_blank"><img src="images/social/instagram.png" style="width: 28px; padding-right: 10px" /></a>
+                    <a href="https://www.github.com/ebrukye" target="_blank"><img src="images/social/github.png" style="width: 28px" /></a>
+                </p>
+            </div>
+            <div class="five columns">
+                <img src="images/pp.jpg" width="100%" />
+            </div>
+            <!-- Menu section -->
+            <div class="twelve columns">
+                <div class="navbar-spacer"></div>
+                <nav class="navbar">
+                    <div class="container">
+                        <ul class="navbar-list">
+                            <li class="navbar-item"><a class="navbar-link" href="#experience">Experience</a></li>
+                            <li class="navbar-item"><a class="navbar-link" href="#education">Education</a></li>
+                            <li class="navbar-item"><a class="navbar-link" href="#skills">Skills</a></li>
+                            <li class="navbar-item"><a class="navbar-link" href="#projects">Projects</a></li>
+                            <li class="navbar-item"><a class="navbar-link" href="#personal">Personal</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+        <div class="row">
+            <!-- Experience section -->
+            <div class="docs-section" id="experience">
+                <h3 class="docs-header">Experience</h3>
+                <div class="docs-example">
+                    <table class="u-full-width">
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <b>Software Engineer</b>
+                                    <br> Sony
+                                    <br>
+                                    <ul>
+                                        <ul>Built the new improved version of Search API, v2 that connects to Solr platform</ul>
+                                        <ul>Responsible for management/development of GTM project</ul>
+                                        <ul>Worked on maintenance of and adding new features to global web sites of Sony Mobile</ul>
+                                    </ul>
+                                </td>
+                                <td>Nov 2017 - Present</td>
+                            </tr>
+                            <tr>
+                                <td><b>IT Intern</b>
+                                    <br>BNP Paribas Cardif</td>
+                                <td>Jul 2015 - Aug 2015</td>
+                            </tr>
+                            <tr>
+                                <td><b>Software Development Intern</b>
+                                    <br>Zet.com</td>
+                                <td>May 2014 - Aug 2014</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <!-- Education section -->
+                <div class="docs-section" id="education">
+                    <h3 class="docs-header">Education</h3>
+                    <div class="docs-example">
+                        <table class="u-full-width">
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <b>Istanbul University</b>
+                                        <br> BSc, Computer Engineering
+                                    </td>
+                                    <td>2012 - 2016</td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <b>Tallinn University</b>
+                                        <br> Exchange Student, Human-Computer Interaction
+                                    </td>
+                                    <td>2015 - 2016</td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <b>Nişantaşı Anadolu Lisesi</b>
+                                        <br> High School
+                                    </td>
+                                    <td>2008 - 2012</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- Skills section -->
+                    <div class="docs-section" id="skills">
+                        <h3 class="docs-header">Skills</h3>I have always been so curious about whole software development cycle, therefore I am trying to improve myself in full-stack development.
+                        <table class="u-full-width">
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <b>PHP, MySQL</b>
+                                    </td>
+                                    <td><span style="font-size: 29px"><b>++++++++</b><span style="color: #ccc">++</span></span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <b>HTML, CSS</b>
+                                    </td>
+                                    <td><span style="font-size: 29px"><b>++++++++</b><span style="color: #ccc">++</span></span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <b>JavaScript</b>
+                                    </td>
+                                    <td><span style="font-size: 29px"><b>+++++++</b><span style="color: #ccc">+++</span></span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <b>Git</b>
+                                    </td>
+                                    <td><span style="font-size: 29px"><b>++++++++</b><span style="color: #ccc">++</span></span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <b>REST API</b>
+                                    </td>
+                                    <td><span style="font-size: 29px"><b>+++++++</b><span style="color: #ccc">+++</span></span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <b>Swift, Firebase</b>
+                                    </td>
+                                    <td><span style="font-size: 29px"><b>+++++</b><span style="color: #ccc">+++++</span></span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <b>Agile Development</b>
+                                    </td>
+                                    <td><span style="font-size: 29px"><b>+++++++++</b><span style="color: #ccc">+</span></span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <b>UI Design</b>
+                                    </td>
+                                    <td><span style="font-size: 29px"><b>++++++</b><span style="color: #ccc">++++</span></span>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <br />
+                        <em><b>To-learn list:</b> React.js, React Native, Design Patterns, SOLID Principles, Laravel, TDD</em>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
