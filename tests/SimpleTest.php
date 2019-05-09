3d20ccd6c893a90d1b65e344833565e941946cbc
Ebru Kaya <ebky14@gmail.com>
2019-05-09 13:18:32 +0300

    travis test

---
+++ tests/SimpleTest.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types = 1);
+
+use PHPUnit\Framework\TestCase;
+
+final class SimpleTest extends TestCase
+{
+    public function testSimple(): void
+    {
+        $this->assertTrue(true);
+    }
+}
