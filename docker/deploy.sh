d9fb9a6aa7e3505b17a23c16fc5464aa53bba880
Ebru Kaya <ebky14@gmail.com>
2019-05-19 16:56:49 +0300

    python update

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -4,7 +4,7 @@ docker --version
 pip install --user awscli
 export PATH=$PATH:$HOME/.local/bin
 
-eval $(aws ecr get-login --region eu-west-1)
+eval $(aws ecr get-login --region $AWS_DEFAULT_REGION)
 
 # build the docker images and push to image repositories
 docker-compose -f docker-compose-prod.yml up -d --build
@@ -12,5 +12,5 @@ docker-compose -f docker-compose-prod.yml up -d --build
 docker tag ebrukayame_web:latest $IMAGE_REPOS_BASE_URL/ebrukayame_web:latest
 docker push $IMAGE_REPOS_BASE_URL/ebrukayame_web:latest
 
-docker tag ebrukayame_php-fpm:latest $IMAGE_REPO_URL/ebrukayame_php-fpm:latest
-docker push $IMAGE_REPO_URL/ebrukayame_php-fpm:latest
+docker tag ebrukayame_php-fpm:latest $IMAGE_REPOS_BASE_URL/ebrukayame_php-fpm:latest
+docker push $IMAGE_REPOS_BASE_URL/ebrukayame_php-fpm:latest
